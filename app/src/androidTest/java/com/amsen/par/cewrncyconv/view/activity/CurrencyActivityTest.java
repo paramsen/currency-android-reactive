81dad97bcca1dee04338a6638a986bc978a87816
Your Name <par.nils.amsen@gmail.com>
2016-08-30 05:07:57 +1000

    espresso tests

---
+++ app/src/androidTest/java/com/amsen/par/cewrncyconv/view/activity/CurrencyActivityTest.java
@@ -0,0 +1,49 @@
+package com.amsen.par.cewrncyconv.view.activity;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.amsen.par.cewrncyconv.R;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * Some small UI tests as per request.
+ *
+ * @author Pär Amsen 2016
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class CurrencyActivityTest {
+    @Rule
+    public ActivityTestRule<CurrencyActivity> rule = new ActivityTestRule<CurrencyActivity>(CurrencyActivity.class);
+
+    @Test
+    public void changeAmount() throws InterruptedException {
+        onView(withText(Matchers.containsString("￥0"))).check(matches(isDisplayed()));
+        onView(withId(R.id.currencyEditText)).perform(ViewActions.typeText("123"));
+        onView(withText(Matchers.containsString("￥0"))).check(doesNotExist());
+    }
+
+    @Test
+    public void changeCurrency() {
+        onView(withText(Matchers.containsString("€"))).check(doesNotExist()); //Shows YEN first
+        onView(withId(R.id.viewPager)).perform(ViewActions.swipeLeft());
+        onView(withText(Matchers.containsString("€"))).check(matches(isDisplayed())); //is EUR showing after swipe?
+    }
+}
